12f9d48191d1d6f8697140f797bad0800157d883
catfrog6502 <catfrog6502@gmail.com>
2025-01-18 20:09:51 -0500

    addnewauthors working

--- addNewAuthorsFromWebsite.py
+++ addNewAuthorsFromWebsite.py
@@ -74,65 +74,34 @@ ijpage = f.read()
 f.close
 
 # split the html into the articles
-articles = ijpage.split("<li class=fragment>")
+articles = ijpage.split("<!-- ArticleSectionStart //-->")
 articles.pop(0) #remove the first element because it's header information, not an article
-
-### extract the titles
-##newtitles = []
-##for i in range(len(articles)):
-##    articles[i] = re.sub("errata.pdf.*</ul>","",articles[i].replace('\n',' '))
-##    articles[i] = re.sub("addendum.pdf.*</ul>","",articles[i].replace('\n',' '))
-##    articles[i] = re.sub(".*blank>","",articles[i].replace('\n',' '))
-##    newtitles.append(articles[i])
-##    
-##    newtitles[i] = re.sub("</a>.*","",newtitles[i]).strip()
-
-
 # extract the author names
-newnames = []
-for i in range(len(articles)):
-    articles[i] = re.sub(".*</a> </b>\s*<ul><li> ","",articles[i].replace('\n',' '))
-    newnames.append(articles[i])
-    newnames[i] = re.sub("<ul>.*","",newnames[i]).strip()
-
-# Split the newnames string into a list of author strings
-for i in range(len(newnames)):
-    tmplist = []
-    words = newnames[i].split(',')
-
-    j=0;
-    while j < len(words):
-        words[j]=words[j].strip()
-##        print(j, end="")
-        if j>0 and re.match("Jr.*|Sr.*", words[j]):
-##            print("FOUND:  ", end="")
-##            print(words)
-            words[j-1] = words[j-1] + " " + words[j]
-            words.pop(j)
-        j=j+1
-    
-    for j in words:
-        j = j.strip()
-        j = re.sub("^and ","",j)
-        if re.search(" and ",j):
-            h = j.split(" and ")
-            tmplist.append(h[0])
-            tmplist.append(h[1])
-        else:
-            tmplist.append(j)
-    newnames[i]=tmplist
-
-
-# make a flat list of the authors of the new papers
 authornames=[]
-nextAuthorID = max(existingAuthors.keys())+1
-for i in newnames:
-    for j in i:
-        authornames.append(lastnamefirst(j))
+
+for i in range(len(articles)):
+    print("=========================")
+    # print(articles[i])
+    # print("=========================")
+
+    tmplist = articles[i].split("<!-- AuthorNameStart //-->")
+    tmplist.pop(0) # remove first element b/c it's not a name
+    for i in range(len(tmplist)):
+        tmplist[i] = re.sub("<!-- AuthorNameEnd //-->.*","",tmplist[i].replace('\n',' '))
+        tmplist[i] = re.sub(",\s*$","",tmplist[i])
+        print(tmplist[i])
+        authornames.append(lastnamefirst(tmplist[i]))
+
 
 # remove duplicates
 authornames = list(set(authornames))
+print("AUTHORNAMES")
+print(authornames)
+print("++++++++++++++++++++++++++++++++++")
+print("++++++++++++++++++++++++++++++++++")
+print("++++++++++++++++++++++++++++++++++")
 newAuthors = []
+nextAuthorID = max(existingAuthors.keys())+1
 
 # Find authors who are not already in authorlist.txt
 for i in authornames:
@@ -166,12 +135,6 @@ if (len(sys.argv)>1):
     else:
         print("Argument " + sys.argv[1] + " not recognized.")
 
-##
-##for i in range(len(newtitles)):
-###    print(str(i) + " " + newtitles[i])
-##    print(str(i) + " "+str(newnames[i]))
-##    print("-------------------------------")
-##
 
         
 
